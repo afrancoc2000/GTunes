f26fa9618635f766a9502900e3ade8983afdff20
afrancoc2000 <afrancoc2000@gmail.com>
2014-04-22 17:27:48 -0500

    Created gtunes app with models Song and Album

---
+++ grails-app/domain/com/gtunes/Album.groovy
@@ -0,0 +1,15 @@
+package com.gtunes
+
+class Album {
+
+	String title
+	static hasMany = [songs:Song]
+	
+    static constraints = {
+    	title blank:false 
+	}
+	
+	String toString() {
+		return title
+	}
+}
